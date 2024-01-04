ac3b5d12ebec9fb17deed059576ba8f6b35cf669
Adewumi <owonikokoadewunmi@gmail.com>
2024-01-04 15:35:15 +0100

    hero landing page

---
+++ frontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import hand_icon from '../Assets 3/hand_icon.png'
+import "./Hero.css"
+import arrow_icon from '../Assets 3/arrow.png'
+import hero_image from '../Assets 3/hero_image.png'
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className="hero-left">
+            <h2>NEW ARRIVALS ONLY</h2>
+            <div>
+                <div className="hero-hand-icon">
+                    <p>new</p>
+                    <img src={hand_icon} alt="" />
+                </div>
+                <p>collections</p>
+                <p>for everyone</p>
+            </div>
+            <div className="hero-latest-btn">
+                <div>Latest Collection</div>
+                <img src={arrow_icon} alt="" />
+            </div>
+
+        </div>
+        <div className="hero-right">
+            <img src={hero_image} alt="" />
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Hero
+
+// After creating the hero Component, 
+// go ahead and mount it 
+// in shop component under pages
--- frontend/src/Pages/LoginSignUp.jsx
+++ frontend/src/Pages/LoginSignUp.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LoginSignUp = () => {
   return (
-    <div>LoginSignUp</div>
+    <div></div>
   )
 }
 
