b5c0668459a913fbe87b84a79b9fd3391a9408dd
Adewumi <owonikokoadewunmi@gmail.com>
2024-01-06 18:12:09 +0100

    the items component

--- frontend/src/Components/Hero/Item/Popular/Popular.jsx
+++ frontend/src/Components/Hero/Item/Popular/Popular.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import './Popular.css'
+import data_product from "../../../Assets 3/data"
+import Item from '../Item'
 
 const Popular = () => {
   return (
-    <div className='popular'></div>
+    <div className='popular'>
+      <h1>POPULAR IN WOMEN</h1>
+      <hr />
+      <div className="popular_item">
+        {data_product.map((item,i) => {
+         return <Item  key={i} id ={item.id} name={item.name} image ={item.image} new_price={item.new_price} old_price={item.old_price} />
+        })}
+      </div>
+    </div>
   )
 }
 
 export default Popular
+
+//we have provided all the props here fro the data file
+// mount it in the shop page
