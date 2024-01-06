66287898941f1c6eaac8bbde67855dda37536824
Adewumi <owonikokoadewunmi@gmail.com>
2024-01-06 18:46:03 +0100

    new collections

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Hero/Item/Popular/Popular'
+import NewCollection from '../Components/NewCollections/NewCollection'
 import Offers from '../Components/Offers/Offers'
 
 const Shop = () => {
@@ -9,6 +10,7 @@ const Shop = () => {
       <Hero/>
       <Popular />
       <Offers />
+      <NewCollection/>
     </div>
   )
 }
