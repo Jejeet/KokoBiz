df594b4b30f5ee238f05844e649fb7c1878dfac2
Adewumi <owonikokoadewunmi@gmail.com>
2024-01-08 23:29:42 +0100

    context

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,10 +1,10 @@
 import React, {createContext} from "react";
-import { createContext } from "react";
+
 import all_product from "../Components/Assets 3/all_product"
 
 export const ShopContext = createContext(null);
 
-ShopContextProvider = (props)=>{
+const ShopContextProvider = (props)=>{
     const contextValue ={all_product};
 
     return(
