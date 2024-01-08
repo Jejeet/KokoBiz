1024b5f131208037aa6cafc5909890587a5c1c4f
Adewumi <owonikokoadewunmi@gmail.com>
2024-01-08 22:25:45 +0100

    newsletter style

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Hero/Item/Popular/Popular'
 import NewCollection from '../Components/NewCollections/NewCollection'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
 import Offers from '../Components/Offers/Offers'
 
 const Shop = () => {
@@ -11,6 +12,7 @@ const Shop = () => {
       <Popular />
       <Offers />
       <NewCollection/>
+      <NewsLetter/>
     </div>
   )
 }
