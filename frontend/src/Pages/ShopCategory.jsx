5ab0732668b6301f432706b20ae5d1552e8801f0
Adewumi <owonikokoadewunmi@gmail.com>
2024-01-10 20:36:01 +0100

    shop category kid

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -7,7 +7,7 @@ import Item from "../Components/Hero/Item/Item"
 
 
 const ShopCategory = (props) => {
-  const {all_product} = useContext(ShopContext)
+  const {all_product} = useContext(ShopContext);
   return (
     <div className='shop-category'>
       {/* html structure for the shop category page */}
@@ -22,7 +22,7 @@ const ShopCategory = (props) => {
       </div>
       <div className="shopcategory-products">
         { all_product.map((item,i)=>{
-          if(props.category === item.category) {
+          if(props.category===item.category) {
             return <Item key= {i} id ={item.id} 
             name={item.name} image ={item.image} 
             new_price={item.new_price} old_price={item.old_price} />
