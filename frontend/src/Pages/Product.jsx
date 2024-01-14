adf5421a27d5e6f3f4ec203414eee92b67c39cfa
Adewumi <owonikokoadewunmi@gmail.com>
2024-01-14 22:41:33 +0100

    Each product page

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useContext } from 'react'
+import { useParams } from 'react-router-dom';
+import { ShopContext } from '../Context/ShopContext'
 
 const Product = () => {
+  //grab all data using context
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find((e)=> e.id === Number(productId))
   return (
     <div></div>
   )
