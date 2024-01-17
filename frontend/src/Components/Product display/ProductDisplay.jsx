735b9a5e8d0cf91b6ab8d35858c2d4ab02b0f5ac
Adewumi <owonikokoadewunmi@gmail.com>
2024-01-17 23:45:37 +0100

    Each product page

--- frontend/src/Components/Product display/ProductDisplay.jsx
+++ frontend/src/Components/Product display/ProductDisplay.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
-import "./PoductDisplay.css  "
+import "./ProductDisplay.css"
+import star_icon from "../Assets 3/star_icon.png"
+import star_dull_icon from "../Assets 3/star_dull_icon.png"
 
-const ProductDisplay = () => {
+const ProductDisplay = (props) => {
+    const {product} = props;
   return (
     <div className='productdisplay'>
-        <div className="productdiplay-left"></div>
-        <div className="producdisplay-right"></div>
+        <div className="productdiplay-left">
+            <div className="productdisplay-img-list">
+                <img src={product.props} alt="" />
+                <img src={product.props} alt="" />
+                <img src={product.props} alt="" />
+                <img src={product.props} alt="" />
+            </div>
+            <div className="productdisplay-img">
+                <img src={product.image} alt="" className="productdisplay-main-img" />
+            </div>
+        </div>
+        <div className="producdisplay-right">
+            <h1>
+                {product.name}
+            </h1>
+            <div className="productdisplay-right-star">
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_dull_icon} alt="" />
+            </div>
+        </div>
     </div>
   )
 }
