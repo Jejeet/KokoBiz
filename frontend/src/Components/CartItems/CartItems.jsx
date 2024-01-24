6ba37453205f6e2ae9330ac225c7fa931728e899
Adewumi <owonikokoadewunmi@gmail.com>
2024-01-24 21:17:24 +0100

    cartitems

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../../Context/ShopContext'
 import "./CartItems.css"
 
 const CartItems = () => {
+  //access the data and functtin through contextAPI
+  const {all_product, CartItems,removeFromCart}= useContext(ShopContext)
   return (
-    <div className='cartitems'></div>
+    <div className='cartitems'>
+      <div className="cartitems-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Quntity</p>
+        <p>Total</p>
+        <p>Remove</p>
+      </div>
+      <hr />
+      <div>
+        <div className="classitems-format"></div>
+      </div>
+    </div>
   )
 }
 
