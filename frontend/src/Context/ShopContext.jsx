82c5bbeb7a1d2e7a1ca70554ba0d2a7d05440163
Adewumi <owonikokoadewunmi@gmail.com>
2024-01-24 20:41:14 +0100

    access cart data

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,11 +1,27 @@
 import React, {createContext} from "react";
+import { useState } from "react";
 
 import all_product from "../Components/Assets 3/all_product"
 
 export const ShopContext = createContext(null);
 
+//for add to cart. using this context, we can access the add to cart data in any component
+
+const getDefaultCart = () =>{
+    let cart = {};
+    for(let index = 0; index < all_product.length+1; index++){
+        cart [index] =0;
+    }
+    return cart;
+}
+
 const ShopContextProvider = (props)=>{
-    const contextValue ={all_product};
+    const [cartItems,setCartItems]= useState(getDefaultCart());
+    const contextValue ={all_product,cartItems};
+    
+
+    
+    
 
     return(
         <ShopContext.Provider value={contextValue}>
