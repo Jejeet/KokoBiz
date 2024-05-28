de44c78d88bfa71c5ba33a50a7a563f4a28042ab
Adewumi <owonikokoadewunmi@gmail.com>
2024-05-28 16:45:23 +0100

    render product with productlist component.

--- src/Components/Hero/Item/Popular/Popular.jsx
+++ src/Components/Hero/Item/Popular/Popular.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import './Popular.css'
 import data_product from "../../../Assets 3/data"
-import Item from '../Item'
+import Section from '../../../layouts/Section'
+import ProductList from '../../../products/ProductList'
 
 const Popular = () => {
   return (
+    <Section>
     <div className='popular'>
       <h1>POPULAR IN WOMEN</h1>
       <hr />
-      <div className="popular_item">
-        {data_product.map((item,i) => {
-         return <Item  key={i} id ={item.id} name={item.name} image ={item.image} new_price={item.new_price} old_price={item.old_price} />
-        })}
-      </div>
+      <ProductList products={data_product}/>
     </div>
+    </Section>
   )
 }
 
