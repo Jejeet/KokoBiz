920a4bcc6786a658ccb4466321812659a09abcb4
Adewumi <owonikokoadewunmi@gmail.com>
2024-05-28 16:58:21 +0100

    extracte product listing template to component

---
+++ src/Components/products/ProductList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './ProductList.css'
+import Product from './Product'
+
+const ProductList = ({products = [], className=''}) => {
+  return (
+        <div className={`products ${className}`}>
+            {products.map((item,i)=>{
+                return <Product key={i} id ={item.id} name={item.name} image ={item.image} new_price={item.new_price} old_price={item.old_price}/>
+
+            })}
+        </div>
+  )
+}
+
+export default ProductList
