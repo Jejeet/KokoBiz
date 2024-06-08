cd988897ac44c1ae03f1fb385d9e9ba74c1fc23f
Adewumi <owonikokoadewunmi@gmail.com>
2024-06-08 22:54:21 +0100

    replaced all image icon with lucide icon . rearranged the nav elements in an easy stylable way. replace use sate with use ref because of mobile responsiveness (to access the DOM element directly)

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,34 @@
 import React from 'react'
 import "./Navbar.css"
-import bar from "../Assets 3/menu icon.png"
-import cross from "../Assets 3/cross icon.jpeg"
 import logo from "../Assets 3/logo.png"
-import cart_icon from "../Assets 3/cart_icon.png"
+
+import {MenuIcon, ShoppingBagIcon} from "lucide-react"
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useContext } from 'react'
 import { ShopContext } from '../../Context/ShopContext'
+import { useRef } from 'react'
 
 const Navbar = () => {
   // usestate so i can move from one slide to another(page)
-  const [showMenu, setShowMenu]= useState(true)
+
   const [menu, setMenu] = useState("shop");
   const {getTotalItems}= useContext(ShopContext);
+  const navMenuRef = useRef()
+
+
+
+  const handleShowMenu = ()=>{
+    navMenuRef.current.classList.toggle("show")
+    
+  }
+
+  const hideNavbarOnClickOnModal = ()=>{
+      navMenuRef.current.classList.remove("show")
+     
+  console.log(navMenuRef.current)
+  }
+
   return(
     <div className='navbar'>
         <div className="nav-logo">
@@ -21,8 +36,9 @@ const Navbar = () => {
             <p>KOKO_GARB</p>
         </div>
        
-      
-        <ul className="nav-menu"> 
+       
+        <div ref={navMenuRef} className={`nav_menu`} onClick={hideNavbarOnClickOnModal}>
+        <ul className="nav-list"> 
         {/* add onclick to each of them and a function set to set menu*/}
         {/* the hr was fist included manually to style on shop but
          then removed and included as a condition using ternary operators
@@ -30,25 +46,33 @@ const Navbar = () => {
          will move to each page of the menu clicked) */}
 
          {/* use the link tag to connect the routes to the nav */}
+
            <li onClick={()=> {setMenu("shop")}}> <Link  style={{textDecoration:'none'}} to = '/'>Shop</Link>  {menu=== "shop"?<hr/>:<></>}</li>
            <li onClick={()=> {setMenu("men")}}><Link style={{textDecoration:'none'}} to = '/men'>Men</Link> {menu=== "men"?<hr/>:<></>}</li>
            <li onClick={()=> {setMenu("women")}}><Link style={{textDecoration:'none'}} to = '/women'>Women</Link>{menu=== "women"?<hr/>:<></>}</li>
            <li onClick={()=> {setMenu("kids")}}><Link style={{textDecoration:'none'}} to ='/kids'>Kids</Link> {menu=== "kids"?<hr/>:<></>}</li> 
         </ul>
-        <div className="nav-login-cart">
-           <Link to='/login'><button>Login</button></Link> 
-           <Link to ='/cart'><img src={cart_icon} alt="" /></Link> 
-            {/* to add a counter to the cart  */}
-            <div className="nav-cart-count">{getTotalItems()}</div>
         </div>
 
-        <div className='navMedia'>
-        {showMenu ? (
-          <img src={bar} alt="" onClick={() => setShowMenu(false)} />
-        ) : (
-          <img src={cross} alt="" onClick={() => setShowMenu(true)} />
-        )}
+        <div className="nav-login-cart">
+           <Link to='/login'><button>Login</button></Link> 
+           <div className='cart'>
+           <Link to ='/cart' className='inline-block'>
+            <ShoppingBagIcon  className='cartBag'/>
+            <span className="nav-cart-count">{getTotalItems()|| 0}</span>
+           </Link> 
+            {/* to add a counter to the cart  */}
+            
+           </div>
+           <div className='navMedia' onClick={handleShowMenu}>
+        <MenuIcon/>
       </div>
+           
+        </div>
+        
+        
+        
+     
 
     </div>
    
