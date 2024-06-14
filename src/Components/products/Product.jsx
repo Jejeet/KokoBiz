b5d0dcf0389b9526a39fffc45fb2db25f7f1cb2a
Adewumi <owonikokoadewunmi@gmail.com>
2024-06-14 23:36:49 +0100

    stored the product name in a ref

--- src/Components/products/Product.jsx
+++ src/Components/products/Product.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { useRef } from 'react'
 import { Link } from 'react-router-dom'
 import './Product.css'
 
 
 const Product = ({id, name, new_price, old_price, image}) => {
+  const menuref = useRef().current
+  
   return (
     <div className='item'>
     <Link to ={`/product/${id}`}> <img onClick={window.scrollTo(0,0)} src={image} alt={name} /></Link>  
-        <p> {name}</p>
+        <p ref={menuref}> {name}</p>
         <div className="item-prices" >
             <div className="item-price-new">
                ${new_price}
