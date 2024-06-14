5b0d3495922ec34e6d7b73a81a6099efab300a60
Adewumi <owonikokoadewunmi@gmail.com>
2024-06-14 23:38:48 +0100

    no adjustment

--- src/Components/products/ProductList.jsx
+++ src/Components/products/ProductList.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+// import ReactDOM from 'react-dom/client';
 import './ProductList.css'
 import Product from './Product'
 
 const ProductList = ({products = [], className=''}) => {
+  
+
   return (
         <div className={`products ${className}`}>
+         
             {products.map((item,i)=>{
                 return <Product key={i} id ={item.id} name={item.name} image ={item.image} new_price={item.new_price} old_price={item.old_price}/>
 
